bb868483560ceaf3bbac7550d8cbf1964cf5e0b1
Andrii Komarnitskyi <ticapac@gmail.com>
2016-12-01 14:38:12 +0200

    add html task (pug,useref), fix async

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,31 @@ const gulp = require('gulp');
 const tasks = require('./tasks');
 const watch = require('gulp-watch');
 const browserSync = require('browser-sync').create();
-const destination = './';
 
+const globs = {
+	styles: ['./src/styles/**/*.pcss'],
+	js: ['./src/js/**/*.js'],
+	html: ['./src/**/*.{html,pug}']
+};
 
-gulp.task('styles', tasks.styles(['./src/styles/**/*.pcss'], './app/css/'));
-gulp.task('js', tasks.scripts(['./src/js/**/*.js'], './app/js'));
+gulp.task('styles', tasks.styles(globs.styles, './app/css/'));
+gulp.task('js', tasks.scripts(globs.js, './app/js'));
+gulp.task('html', tasks.html(globs.html, './app/'));
+
+gulp.task('build', gulp.series('styles', 'js', 'html',(done) => done()));
+
 
 //define watchers
-gulp.task('serve', () => {
-  browserSync.init({
-    server: "./",
-    injectChanges: true,
-    reloadDebounce: 500
-  });
-  return watch('./styles/**/*.pcss', gulp.series(['pcss']));
+gulp.task('serve', gulp.series('build', (done) => {
+
+	browserSync.init({
+		server: "./app/",
+		injectChanges: true,
+		reloadDebounce: 500
+	}, done);
+	watch(globs.styles, gulp.series(['styles']));
+	watch(globs.js, gulp.series(['js']));
+	watch(globs.html, gulp.series(['html']));
 
 
-});
-
-
-gulp.task('build', () => {
-  gulp.series(['styles', 'js']);
-});
+}));
